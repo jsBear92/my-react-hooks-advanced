5e32e2b19e10741b8638ff21011afd4ac9f489ba
jsBear92 <js104427@gmail.com>
2023-05-24 19:23:29 +0800

    short-circuit challenge

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const [name, setName] = useState('');
+
+  const login = () => {
+    setUser(name);
+  }
+
+  const logout = () => {
+    setName('');
+    setUser(null);
+  }
+
+  return (
+    <div>
+      <h1>{user ?  `Hello ${user}` : 'Welcome'}</h1>
+      {user && <button onClick={logout}>Logout</button>}
+      {!user && <div>
+          <input type="text" placeholder="Enter your name" onChange={(e) => setName(e.target.value)} />
+          <button type="button" onClick={login}>Login</button>
+        </div>}
+    </div>
+  );
 };
 
 export default UserChallenge;
