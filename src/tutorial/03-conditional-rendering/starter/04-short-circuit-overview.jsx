8e6cc6e66d4083b4a4b9ac45c9434851fbe53dc4
Jaeseong Jeong <js104427@gmail.com>
2023-05-24 14:34:20 +0800

    short circuit basic

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,16 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [text, setText] = useState('');
+  const [name, setName] = useState('susan');
+
+  return (
+    <div>
+      <h4>Falsy OR : {text || 'hello'}</h4>
+      <h4>Falsy AND : {text && 'hello'}</h4>
+      <h4>Truthy OR : {name || 'hello'}</h4>
+      <h4>Truthy AND : {name && 'hello'}</h4>
+    </div>
+  );
 };
 export default ShortCircuitOverview;
