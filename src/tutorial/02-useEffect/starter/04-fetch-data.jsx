2f41218342bdc715e8344baee7e5cca605ebc759
Jaeseong Jeong <js104427@gmail.com>
2023-05-24 13:56:42 +0800

    useEffect - fetch, async, await or then

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,41 @@
+import { useState, useEffect } from 'react';
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await fetch(url)
+        const users = await res.json();
+        setUsers(users);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchUser();
+  }, [])
+
+  return (
+    <div>
+      <h2>Fetch Data Example</h2>
+      <h3>Github Users</h3>
+      <ul className='users'>
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              <img src={user.avatar_url} alt={user.login}/>
+              <div>
+                <h5>{user.login}</h5>
+                <a href={user.url}>Profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 export default FetchData;
