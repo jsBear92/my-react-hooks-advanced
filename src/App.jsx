219690e1f41f2c41490277942fe00d4a99b9e331
Jaeseong Jeong <js104427@gmail.com>
2023-05-24 13:07:13 +0800

    useState - handle object

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/01-useState/starter/03-useState-array';
+import Starter from './tutorial/01-useState/starter/04-useState-object';
 // import Final from './tutorial/01-useState/final/01-error-example';
 
 function App() {
--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,30 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'perter',
+    age: 24,
+    hobby: 'read books'
+  })
+
+  const displayPerson = () => {
+    // overwrite all elements
+    setPerson({ name: 'john', age: 28, hobby: 'coding' })
+    // overwrite only name
+    // setPerson({ ...person, name: 'john' })
+
+  };
+
+  return (
+    <div>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>Enjoys: {person.hobby}</h3>
+      <button type='button' className='btn' onClick={displayPerson}>
+        show John
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
