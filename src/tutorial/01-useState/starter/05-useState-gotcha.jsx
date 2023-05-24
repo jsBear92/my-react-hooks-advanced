9507141636011299e9983d3b850653707dfd4c7a
Jaeseong Jeong <js104427@gmail.com>
2023-05-24 13:17:16 +0800

    useState - prevState

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  // this way cannot show current value immediately
+  // const handleClick = () => {
+  //   setValue(value + 1);
+  // };
+
+  // this way can show current value immediately
+  //  const handleClick = () => {
+  //   setValue((prevValue) => {
+  //     const newValue = prevValue + 1;
+  //     return newValue;
+  //   });
+  // }
+
+  // timeout
+  const handleClick = () => {
+    console.log("Click!");
+    setTimeout(() => {
+      setValue((prevValue) => {
+        const newValue = prevValue + 1;
+        return newValue;
+      });
+      console.log("Clicked!");
+    }, 3000);
+  }
+
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        Click
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
