b85f715de9e2b34f62cc464835f0a84cf0854764
jsBear92 <js104427@gmail.com>
2023-05-24 20:56:47 +0800

    Named exports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge';
-// import Final from './tutorial/01-useState/final/01-error-example';
+import { Home, About } from "./tutorial/05-leverage-javascript/starter/Pages";
+
 
 function App() {
 
   return (
     <div className="container">
-        <Starter />
+        <Home />
+        <About />
     </div>
   );
 }
