d85e7056d7701299f6c76e81fe5cf4f9dc72e0c6
Jaeseong Jeong <js104427@gmail.com>
2023-05-24 12:26:04 +0800

    array and filter function

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,44 @@
+import { useState } from 'react';
+import { data } from '../../../data';
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  }
+
+  const clearItems = () => {
+    setPeople([]);
+  }
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button
+              type='button'
+              onClick={ () => removeItem(id) }
+            >
+              remove
+            </button>
+          </div>
+        )
+      })}
+      <button
+        type='button'
+        className='btn'
+        style={{ marginTop: '2rem' }}
+        onClick={ clearItems }
+      >
+        clear items
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
