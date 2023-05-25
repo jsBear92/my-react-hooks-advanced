06ad9aab11c0fd89d4b509e7e4263d1146fe55a2
Jaeseong Jeong <js104427@gmail.com>
2023-05-25 12:21:36 +0800

    Controlled Input - onChange, onSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Example from "./tutorial/05-leverage-javascript/starter/Example";
+import Starter from "./tutorial/06-forms/starter/01-controlled-inputs";
 
 
 function App() {
 
   return (
     <div className="container">
-        <Example />
+        <Starter />
     </div>
   );
 }
