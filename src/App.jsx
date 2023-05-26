ce7dc47d323c4f56d29394fe3451ca589ebc98c2
jsBear92 <js104427@gmail.com>
2023-05-26 17:12:25 +0800

    prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
-import Starter from "./tutorial/08-custom-hooks/starter/02-fetch-data";
-
+import Starter from './tutorial/09-context-api/starter/Navbar';
 
 function App() {
-
   return (
-    <div className="container">
-        <Starter />
+    <div>
+      <Starter />
     </div>
   );
 }
 
-export default App
+export default App;
